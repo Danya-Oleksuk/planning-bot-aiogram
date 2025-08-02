a69dd5dac8550a8e9e53efdbbb91bce3243a29ed
Danya Oleksuk <sinrandomg@gmail.com>
2025-08-02 21:54:28 +0300

    Add user_stats table with functions to insert and update task statistics

--- database/postgres.py
+++ database/postgres.py
@@ -27,6 +27,16 @@ async def create_telegram_bot_db():
                 unique_key TEXT UNIQUE 
             ) 
         ''')
+    
+    await conn.execute('''
+        CREATE TABLE IF NOT EXISTS user_stats (
+            id SERIAL PRIMARY KEY,
+            user_id BIGINT UNIQUE REFERENCES users(telegram_id) ON DELETE CASCADE,
+            total_tasks INTEGER DEFAULT 0,
+            completed_tasks INTEGER DEFAULT 0
+        )
+    ''')
+    await conn.close()
 
 async def is_user_in_database(telegram_id: int) -> bool:
     async with pool.acquire() as conn:
@@ -41,7 +51,30 @@ async def create_user(user_id: int, first_name: str, last_name: str, username: s
                 INSERT INTO users (telegram_id, first_name, last_name, username, is_vip, joined_at, unique_key)
                 VALUES ($1, $2, $3, $4, $5, $6, $7)
             ''', user_id, first_name, last_name, username, is_vip, datetime.datetime.now(), unique_key)
+
+async def get_user_stats(user_id: int):
+    async with pool.acquire() as conn:
+        user_stats = await conn.fetchrow('SELECT total_tasks, completed_tasks FROM user_stats WHERE user_id = $1', user_id)
+        return dict(user_stats) if user_stats else None
+
+async def increment_total_tasks(user_id: int):
+    async with pool.acquire() as conn:
+        exists = await conn.fetchval('SELECT 1 FROM user_stats WHERE user_id = $1', user_id)
         
+        if not exists:
+            await conn.execute('INSERT INTO user_stats(user_id, total_tasks) VALUES($1, 1)', user_id)
+        else:
+            await conn.execute('UPDATE user_stats SET total_tasks = total_tasks + 1 WHERE user_id = $1', user_id)
+
+async def increment_completed_tasks(user_id: int):
+    async with pool.acquire() as conn:
+        exists = await conn.fetchval('SELECT 1 FROM user_stats WHERE user_id = $1', user_id)
+
+        if not exists:
+            await conn.execute('INSERT INTO user_stats(user_id, completed_tasks) VALUES($1, 1)', user_id)
+        else:
+            await conn.execute('UPDATE user_stats SET completed_tasks = completed_tasks + 1 WHERE user_id = $1', user_id)
+                    
 async def user_blocked_bot(user_id: int):
     async with pool.acquire() as conn:
         conn.execute('UPDATE users SET is_banned = TRUE WHERE telegram_id = $1', user_id)
